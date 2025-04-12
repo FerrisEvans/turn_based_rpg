6352a9d499447e5ddfeb6df7f62a4576b0974740
FerrisEvans <ferris.evans@outlook.com>
2025-04-12 16:26:11 +0800

    move

--- scenes/player/leader.gd
+++ scenes/player/leader.gd
@@ -1,21 +1,46 @@
 extends CharacterBody2D
 
-var direction = Vector2(1, -0.5)
-@export var max_speed: int = 200
+var direction: Vector2
+var previous_direction: String
+var current_direction: String
+@export var speed: int = 200
+
+const UP = "up"
+const DOWN = "down"
+const LEFT = "left"
+const RIGHT = "right"
 
 func _physics_process(delta: float) -> void:
-	var speed = max_speed
-	if Input.is_action_pressed("up"):
+	global_position += move() * delta
+
+
+func move() -> Vector2:
+	direction = Vector2.ZERO
+	if Input.is_action_just_pressed(UP):
+		previous_direction = current_direction
+		current_direction = UP
+	if Input.is_action_just_pressed(DOWN):
+		previous_direction = current_direction
+		current_direction = DOWN
+	if Input.is_action_just_pressed(LEFT):
+		previous_direction = current_direction
+		current_direction = LEFT
+	if Input.is_action_just_pressed(RIGHT):
+		previous_direction = current_direction
+		current_direction = RIGHT
+
+	if (Input.is_action_just_released(UP) and current_direction == UP) \
+		or (Input.is_action_just_released(DOWN) and current_direction == DOWN) \
+		or (Input.is_action_just_released(LEFT) and current_direction == LEFT) \
+		or (Input.is_action_just_released(RIGHT) and current_direction == RIGHT):
+		current_direction = previous_direction
+
+	if Input.is_action_pressed(UP) and current_direction == UP:
 		direction = Vector2(1, -0.5)
-	elif Input.is_action_pressed("down"):
+	if Input.is_action_pressed(DOWN) and current_direction == DOWN:
 		direction = Vector2(-1, 0.5)
-	elif Input.is_action_pressed("left"):
+	if Input.is_action_pressed(LEFT) and current_direction == LEFT:
 		direction = Vector2(-1, -0.5)
-	elif Input.is_action_pressed("right"):
+	if Input.is_action_pressed(RIGHT) and current_direction == RIGHT:
 		direction = Vector2(1, 0.5)
-	else:
-		speed = 0
-	
-	global_position += direction * speed * delta
-	#velocity = direction * speed
-	#move_and_slide()
+	return direction * speed
