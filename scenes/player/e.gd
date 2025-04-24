31221c4ebc7ccc703d2e01fc9f1d61d78c557554
FerrisEvans <ferris.evans@outlook.com>
2025-04-24 21:44:09 +0800

    Update e.gd

--- scenes/player/e.gd
+++ scenes/player/e.gd
@@ -1,15 +1,12 @@
 extends CharacterBody2D
 class_name E
 
-
 signal battle_start(battle_enemy: Array[Enemy], leader: Leader)
 
 var battle_enemy: Array[Enemy] = []
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	# emit_signal("battle_start", battle_enemy, body)
 	if body is CharacterBody2D and body.name == "Leader":
-		print("_on_area_2d_body_entered")
 		var leader = body as Leader
 		if leader:
 			var tween = create_tween()
